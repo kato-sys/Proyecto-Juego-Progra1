2c03355972f1f343b265d4d77fc639c87d78bd0d
DiegoCamMar <diegocamachomartinez26@gmail.com>
2024-10-30 14:28:46 -0600

    inventario cambiado para tener un vector de items, un item arma y un item armadura, y los metodos printInventario y addInventario fueron cambiados acorde.

--- Inventario.java
+++ Inventario.java
@@ -5,52 +5,43 @@ public class Inventario
      * Aquí se guardan los objetos ítems y armas ya preexistentes 
      * en vectores que funcionan como el inventario
      */
-    private Item[] inventario_items;
-    private Item[] inventario_armas;
-    /**
-     * Las variables cuant@s llevan cuenta de la cantidad de objetos en el arreglo, para ahorrarnos revisar los espacios null
-     */
+    public Item[] inventario_items;
+    public Item arma_equipped;
+    public Item armadura_equipped;
+
+    
+    //lleva cuenta de la cantidad de items en el arreglo, para ahorrarnos revisar los espacios null
     private int cuantosItems;
-    private int cuantasArmas;
 
     public Inventario()
     {
         inventario_items = new Item[12];
-        inventario_armas = new Item[12];
-        cuantosItems = -1;
-        cuantasArmas = -1;
+
     }
 
     
-      public void printInventario(boolean imprimirItems) //booleano es para impriir o items o armas
+      public void printInventario()
       {
-        if(imprimirItems)
-        {
             for(int i = 0; i < inventario_items.length; i++)
             {
                 System.out.println(inventario_items[i].getNombre() + ": " + inventario_items[i].getDescripcion() + "\n DEF boost: " + inventario_armas[i].getPoder());
             }
-        }
-        else
-        {
-            for(int i = 0; i < inventario_armas.length; i++)
-            {
-                System.out.println(inventario_armas[i].getNombre() + ": " + inventario_armas[i].getDescripcion() + "\n ATK boost: " + inventario_armas[i].getPoder());
-            }
-        }
      }
 
-    public void addInventario(Item objeto, boolean esItem) //nuevamente, booleano es para saber si el objeto a agregar es un item o un arma
+    public void addInventario(Item objeto, String tipo_item) //para saber si es item, arma o armadura
      {
-         if(esItem)
+        switch(tipo_item)
         {
-            cuantosItems++;
-            inventario_items[cuantosItems] = objeto;
-        }
-        else
-        {
-            cuantasArmas++;
-            inventario_items[cuantasArmas] = objeto;
+            case "item":
+                cuantosItems++;
+                inventario_items[cuantosItems] = objeto;
+                break;
+            case "arma":
+                arma_equipped = objeto;
+                break;
+            case "armadura":
+            armadura_equipped = objeto;                
+            break;
         }
      }
 
